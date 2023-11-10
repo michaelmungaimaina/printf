ca2696868f4721afefaf35ffeeb9c6276f0830a5
johnpeter254 <johnpeter7773@gmail.com>
2023-11-10 09:09:41 -0800

    c, s, & % format specifier

--- _printf.c
+++ _printf.c
@@ -1,7 +1,54 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdarg.h>
 
-/**  **/
+/**
+ * _printf - prints character & string to std output
+ * @format: c & s format specifier
+ *
+ * Return: count
+ */
 
 int _printf(const char * const format, ...){
+	va_list args;
+	va_start(args, format);
 
+	int count = 0;
+
+	while (*format != '\0')
+	{
+		if (*format == '%')
+		{
+			format++;
+
+			switch (*format)
+			{
+				case 'c':
+					putchar(va_arg(args, int));
+					count++;
+
+					break;
+				case 's':
+					count += printf("%s", va_arg(args, char *));
+					break;
+				case '%':
+					putchar('%');
+					count++;
+					break;
+				default:
+					putchar('%');
+					putchar(*format);
+					count += 2;
+			}
+		}
+		else
+		{
+			putchar(*format);
+			count++;
+		}
+		format++;
+	}
+
+	va_end(args);
+	return (count);
 }
