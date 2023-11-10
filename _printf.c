da705753be5569023d13bd556c306b7d938f1628
maina michael <pantalemonmongah@gmail.com>
2023-11-10 18:46:38 +0300

    Update file _printf.c

---
+++ _printf.c
@@ -0,0 +1,45 @@
+#include "main.h"
+/**
+ * _printf - is a function that calls the correct prototype function to print.
+ * @format: identifier to look for.
+ * Return: the length of the string.
+ */
+int _printf(const char * const format, ...)
+{
+	/** Conversion specifier and function pointer **/
+	convert_match m[] = {
+		{"%i", printf_int},
+		{"%d", printf_dec}
+	};
+
+	/** Holds variable arguments **/
+	va_list args;
+	int i = 0, j, len = 0;
+
+	va_start(args, format);
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+		return (-1);
+
+Here:
+	/** format processing **/
+	while (format[i] != '\0')
+	{
+		j = 13;
+		while (j >= 0)
+		{
+			if (m[j].id[0] == format[i] && m[j].id[1] == format[i + 1])
+			{
+				len += m[j].f(args);
+				i = i + 2;
+				goto Here;
+			}
+			j--;
+		}
+		/** default case **/
+		_putchar(format[i]);
+		len++;
+		i++;
+	}
+	va_end(args);
+	return (len);
+}
