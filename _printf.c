5d3bd452e8ff893dbbb85ac8df5417ee0f8f9569
maina michael <pantalemonmongah@gmail.com>
2023-11-13 16:13:03 +0300

    Fix: fun _printf declaration

--- _printf.c
+++ _printf.c
@@ -6,7 +6,7 @@
  * @format: identifier to look for.
  * Return: the length of the string.
  */
-int _printf(const char * const format, ...)
+int _printf(const char *format, ...)
 {
 	/** Conversion specifier and function pointer **/
 	call_match m[] = {
